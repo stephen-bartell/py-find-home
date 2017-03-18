f43ce45b3d13874a501b9401663a7c9e1250362a
Stephen Bartell <stephen.bartell@sdvi.com>
2017-03-18 10:22:59 -0700

    initial commit

---
+++ test/test_find_home_location.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+
+class FindHomeTestCase(TestCase):
+
+    def test_home_found(self):
+        pass
+
+    def test_home_not_found(self):
+        pass
---
+++ test/test_visit.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+
+class VisitTestCase(TestCase):
+
+    def test_get_duration_when_in_full_window(self):
+        pass
+
+    def test_get_duration_when_in_partial_window(self):
+        pass
+
+    def test_get_duration_when_not_in_window(self):
+        pass
